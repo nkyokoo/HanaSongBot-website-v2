2a7b5e53350509490f59baf2bff9fd85a825258a
emily <notorious.droid@gmail.com>
2017-12-18 22:41:57 +0100

    working on documentation site

--- commands/assets/index.js
+++ commands/assets/index.js
@@ -1,8 +1,7 @@
-function hide() {
-    var x = document.getElementById("Command-container");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
 }
